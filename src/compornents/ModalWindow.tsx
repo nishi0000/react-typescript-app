bbb93654c6db31b43e48f901835fd3497acb6e7f
nishi0000 <142460496+nishi0000@users.noreply.github.com>
2023-10-27 18:45:47 +0900

    Add files via upload

---
+++ src/compornents/ModalWindow.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+import { Button } from "./Button";
+import "./modalwindow.css";
+
+type ModalProps = {
+    children: String;
+    onClick:Function;
+  }
+
+export const ModalWindow: FC<ModalProps>  = ({children,onClick}) => {
+
+    return (
+        <div className="modal-overlay">
+        <div className="modal-content">
+          <p>{children}</p>
+          <Button onClick={onClick}>閉じる</Button>
+        </div>
+      </div>
+
+    );
+
+
+}
